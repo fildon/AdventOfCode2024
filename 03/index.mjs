fd5e79e303c524c6fe908a3a9f8eaadff8b0c984
Rupert Foggo McKay <maxfmckay@gmail.com>
2024-12-03 17:08:33 +0100

    Day 03 Part 02 SOLVED

--- 03/index.mjs
+++ 03/index.mjs
@@ -12,5 +12,24 @@ export const solvePart1 = (inputLines) =>
  * @param {string[]} inputLines
  */
 export const solvePart2 = (inputLines) => {
-  // TODO
+  const input = inputLines.join("/n");
+  const dos = [...input.matchAll(/do\(\)/g)].map((match) => match.index);
+  const donts = [...input.matchAll(/don't\(\)/g)].map((match) => match.index);
+
+  const matches = [...input.matchAll(/mul\((\d+),(\d+)\)/g)].filter((match) => {
+    // Include this match if it is prior to any dont command
+    if (match.index < donts[0]) return true;
+    // Otherwise we need to check whether a do or dont was most recent
+    const mostRecentDo = dos.filter((doIndex) => doIndex < match.index).at(-1);
+    const mostRecentDont = donts
+      .filter((dontIndex) => dontIndex < match.index)
+      .at(-1);
+
+    // We most recently saw a do command
+    return mostRecentDo > mostRecentDont;
+  });
+
+  return matches
+    .map(([_, a, b]) => parseInt(a) * parseInt(b))
+    .reduce((acc, curr) => acc + curr);
 };
--- 03/test.mjs
+++ 03/test.mjs
@@ -9,17 +9,20 @@ import { solvePart1, solvePart2 } from "./index.mjs";
 describe("Day 03", () => {
   describe("Part 01", () => {
     test("test input", () =>
-      strictEqual(solvePart1(getInputStrings("./03/input-test.txt")), 161));
+      strictEqual(solvePart1(getInputStrings("./03/input-test1.txt")), 161));
     test("real input", () =>
       strictEqual(
         solvePart1(getInputStrings("./03/input-real.txt")),
         159892596
       ));
   });
-  describe.skip("Part 02", () => {
+  describe("Part 02", () => {
     test("test input", () =>
-      strictEqual(solvePart2(getInputStrings("./03/input-test.txt")), -1));
+      strictEqual(solvePart2(getInputStrings("./03/input-test2.txt")), 48));
     test("real input", () =>
-      strictEqual(solvePart2(getInputStrings("./03/input-real.txt")), -1));
+      strictEqual(
+        solvePart2(getInputStrings("./03/input-real.txt")),
+        92626942
+      ));
   });
 });
