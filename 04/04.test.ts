76e69db002c3f46367f70f68143a515e8427e098
Rupert Foggo McKay <maxfmckay@gmail.com>
2024-12-04 16:11:01 +0100

    Day 04 Part 02 SOLVED

--- 04/04.test.ts
+++ 04/04.test.ts
@@ -2,7 +2,7 @@ import { expect, test, describe } from "bun:test";
 
 import { getInputStrings } from "../utils.js";
 
-import { solvePart1 } from "./04.ts";
+import { solvePart1, solvePart2 } from "./04.ts";
 
 describe("Day 04", () => {
   describe("Part 01", () => {
@@ -13,6 +13,16 @@ describe("Day 04", () => {
     test("real input", () =>
       getInputStrings("./04/input-real.txt").then((inputLines) =>
         expect(solvePart1(inputLines)).toBe(2500)
+      ));
+  });
+  describe("Part 02", () => {
+    test("test input", () =>
+      getInputStrings("./04/input-test.txt").then((inputLines) =>
+        expect(solvePart2(inputLines)).toBe(9)
+      ));
+    test("real input", () =>
+      getInputStrings("./04/input-real.txt").then((inputLines) =>
+        expect(solvePart2(inputLines)).toBe(1933)
       ));
   });
 });
--- 04/04.ts
+++ 04/04.ts
@@ -24,12 +24,38 @@ const isXMAS = (
   return true;
 };
 
-const scoreCell = (wordSearch: string[], location: [number, number]) =>
+const countXMASes = (wordSearch: string[], location: [number, number]) =>
   directions.filter((dir) => isXMAS(wordSearch, location, dir)).length;
 
 export const solvePart1 = (inputLines: string[]) =>
   inputLines
     .flatMap((inputLine, y) =>
-      [...inputLine].map((_, x) => scoreCell(inputLines, [x, y]))
+      [...inputLine].map((_, x) => countXMASes(inputLines, [x, y]))
+    )
+    .reduce((acc, curr) => acc + curr);
+
+const isCrossMAS = (wordSearch: string[], [x, y]: [number, number]) => {
+  if (wordSearch[y]?.[x] !== "A") return false;
+
+  const topLeft = wordSearch[y - 1]?.[x - 1];
+  const topRight = wordSearch[y - 1]?.[x + 1];
+  const bottomLeft = wordSearch[y + 1]?.[x - 1];
+  const bottomRight = wordSearch[y + 1]?.[x + 1];
+
+  const positiveDiagonal = [bottomLeft, topRight];
+  const negativeDiagonal = [topLeft, bottomRight];
+
+  if (!positiveDiagonal.includes("M")) return false;
+  if (!positiveDiagonal.includes("S")) return false;
+  if (!negativeDiagonal.includes("M")) return false;
+  if (!negativeDiagonal.includes("S")) return false;
+  return true;
+};
+
+export const solvePart2 = (inputLines: string[]) =>
+  inputLines
+    .map(
+      (inputLine, y) =>
+        [...inputLine].filter((_, x) => isCrossMAS(inputLines, [x, y])).length
     )
     .reduce((acc, curr) => acc + curr);
